a1d75055010cc2f13e23ecde15b8f4e1c4be20f4
hpc course <hpc@gmail.com>
2016-04-27 17:20:40 -0500

    Gray Scale

--- convImg/imgSobel/imgSobel.cpp
+++ convImg/imgSobel/imgSobel.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     
     Size sizeimg = image.size();
   
-    Mat gray_image_opencv;
+    Mat gray_image_opencv, grad_x;
     cvtColor(image, gray_image_opencv, CV_BGR2GRAY);
     Sobel(gray_image_opencv,grad_x,CV_8UC1,1,0,3,1,0,BORDER_DEFAULT);
 		
