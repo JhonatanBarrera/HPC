6d42146743db4a265a79a34c99bf55a0252cb4a7
hpc course <hpc@gmail.com>
2016-04-27 16:49:03 -0500

    Gray Scale

---
+++ convImg/grayScale/build/CMakeFiles/imgGrayScale.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/imgGrayScale.dir/imgGrayScale.cpp.o"
+  "imgGrayScale.pdb"
+  "imgGrayScale"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/imgGrayScale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
